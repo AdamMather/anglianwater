41bbf4b14999a274418c3f1ad91d023b599b35e0
Adam Mather <adam.mather@atos.net>
2016-11-29 19:31:32 +0000

    img/usr geotag - image display

--- public/script/app.js
+++ public/script/app.js
@@ -6,7 +6,8 @@
         'pammSkeleton.controllers',
         'pammSkeleton.services',
         'ngRoute',
-        'ngMessages'
+        'ngMessages',
+        'ngFileUpload'
          ]);
 
     app.config(['$routeProvider', function($routeProvider) {
@@ -15,14 +16,55 @@
                     templateUrl: 'assets/partials/home.html',
                     controller: "HomeCtrl"
                 })
+           .when('/upload', {
+                    templateUrl: 'assets/partials/upload.html',
+                    controller: "HomeCtrl"
+                })
                 .when('/about', {
                     templateUrl: 'assets/partials/about.html',
                     controller: "AboutCtrl"
                 })
                 .otherwise({
-                    redirectTo: '/home'
+                    redirectTo: '/upload'
                 });
           }]);
+
+          app.run(function($rootScope, NextBackBasicService){
+            $rootScope.goNext = function() {
+              NextBackBasicService.goNext();
+            };
+
+            $rootScope.goBack = function() {
+              NextBackBasicService.goBack();
+            };
+          });
+
+          app.factory('NextBackBasicService', function($route, $location) {
+            //array for keeping defined routes
+            var routes = [];
+
+            angular.forEach($route.routes, function(config, route) {
+              //not to add same route twice
+              if (angular.isUndefined(config.redirectTo)) {
+                routes.push(route);
+              }
+            });
+
+            return {
+              goNext: function() {
+                var nextIndex = routes.indexOf($location.path()) + 1;
+                if (nextIndex !== routes.length) {
+                  $location.path(routes[nextIndex]);
+                }
+              },
+              goBack: function() {
+                var backIndex = routes.indexOf($location.path()) - 1;
+                if (backIndex !== -1) {
+                  $location.path(routes[backIndex]);
+                }
+              }
+            };
+          });
 
     angular.module('pammSkeleton.services', []);
     angular.module('pammSkeleton.controllers', []);
--- public/script/controllers/homeController.js
+++ public/script/controllers/homeController.js
@@ -2,8 +2,8 @@
 
 var module = angular.module('pammSkeleton.controllers');
 
-module.controller('HomeCtrl', [ '$http', '$log', '$scope', '$window',
-    function($http, $log, $scope, $window) {
+module.controller('HomeCtrl', [ '$http', '$log', '$scope', '$location', '$timeout', '$window', 'Upload',
+    function($http, $log, $scope, $location, $timeout, $window, Upload) {
 
             var vm = this;
 
@@ -46,6 +46,39 @@ module.controller('HomeCtrl', [ '$http', '$log', '$scope', '$window',
             }
 
             vm.init();
+
+            vm.uploadFile = function(picFile) {
+			    $log.debug("Submit selected ");
+
+			    picFile.upload = Upload.upload({
+				    url : 'ws/submit',
+
+				    data : {
+					    metadata : uploadCtl.metadata,
+					    imageFile : picFile
+				    }
+			    });
+
+			    picFile.upload.then(function(response) {
+
+				    $log.debug("Response received");
+				    picFile.result = response.data;
+				    $location.path("/");
+
+			    }, function (response) {
+				    if (response.status > 0) {
+					    $scope.errorMsg = {
+						    code : response.status,
+						    responseBody: response
+					    };
+
+					    $log.error($scope.errorMsg);
+				    }
+			       }, function (evt) {
+				        // Math.min is to fix IE which reports 200% sometimes
+				        picFile.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+				    });
+		    };
 
             vm.addApplicant = function  () {
 
@@ -115,6 +148,22 @@ module.controller('HomeCtrl', [ '$http', '$log', '$scope', '$window',
                 var file = files[0];
                 var reader = new FileReader; // use HTML5 file reader to get the file
 
+                //upload code
+                var uploadUrl = "/upload";
+                var fd = new FormData();
+                //Take the first selected file
+                fd.append("file", files[0]);
+
+                $http.post(uploadUrl, fd, {
+                    withCredentials: true,
+                    headers: {'Content-Type': undefined },
+                    transformRequest: angular.identity
+                });
+
+                vm.getUserLocation();
+
+                // end upload code
+
                 var img = document.createElement("IMG");
                 reader.onload = function(e){
                     img.setAttribute("src",reader.result);
@@ -128,33 +177,96 @@ module.controller('HomeCtrl', [ '$http', '$log', '$scope', '$window',
                         //Convert coordinates to WGS84 decimal
                         var latRef = EXIF.getTag(this,"GPSLatitudeRef") || "N";
                         var lonRef = EXIF.getTag(this,"GPSLongitudeRef") || "W";
-                        lat = (lat[0] + lat[1]/60 + lat[2]/3600) * (latRef == "N" ? 1 : -1);
-                        lon = (lon[0] + lon[1]/60 + lon[2]/3600) * (lonRef == "W" ? -1 : 1);
+                        var imgLatitude = (lat[0] + lat[1]/60 + lat[2]/3600) * (latRef == "N" ? 1 : -1);
+                        var imgLongitude = (lon[0] + lon[1]/60 + lon[2]/3600) * (lonRef == "W" ? -1 : 1);
 
                         //Send the coordinates to your map
-                        vm.setGeotagFields(lat,lon,EXIF.pretty(img));
+                        var location = vm.getGeoLocation(imgLatitude,imgLongitude);
+                        var imageMap = 'imageMap';
+                        vm.initMap(imageMap, location);
+
+                        // display map co-ordinates
+                        document.getElementById("imgLatitude").value = imgLatitude;
+                        document.getElementById("imgLongitude").value = imgLongitude;
+
+                        //vm.imgLatitude = imgLatitude;
+                        //vm.imgLongitude = imgLongitude;
+
                     });
                 };
                 reader.readAsDataURL(file);
 
             }
 
-            vm.setGeotagFields = function  (lat, lon, geodata) {
-
-                document.getElementById("lat").value = lat;
-                document.getElementById("lon").value = lon;
-
-                vm.latitude = lat;
-                vm.longitude = lon;
+            vm.getGeoLocation = function  (lat, lon) {
 
                 var location = {lat: lat, lng: lon};
 
-                vm.initMap(location);
+                return location;
+
             }
 
-            vm.initMap = function (location) {
+            vm.initMap = function (eleId, location) {
 
-                $window.map = new google.maps.Map(document.getElementById('map'), { zoom: 8, center: location });
+                //var eleId = 'imageMap';
+
+                $window.map = new google.maps.Map(document.getElementById(eleId), { zoom: 8, center: location });
                 var marker = new google.maps.Marker({ position: location, map: map });
             }
+
+             vm.getUserLocation = function () {
+                console.log("getting location");
+                //check if the geolocation object is supported, if so get position
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(vm.displayLocation, vm.displayError);
+                } else {
+                    document.getElementById("locationData").innerHTML = "Sorry - your browser doesn't support geolocation!";
+                    //console.log("Sorry - your browser doesn't support geolocation!");
+                }
+            };
+
+
+            vm.displayLocation = function (position) {
+
+                var usrLatitude = position.coords.latitude;
+                var usrLongitude = position.coords.longitude;
+
+                //Send the coordinates to your map
+                var location = vm.getGeoLocation(usrLatitude,usrLongitude);
+                var userMap = 'userMap';
+                vm.initMap(userMap, location);
+                // display map co-ordinates
+                document.getElementById("usrLatitude").value = usrLatitude;
+                document.getElementById("usrLongitude").value = usrLongitude;
+
+                //vm.usrLatitude = usrLatitude;
+                //vm.usrLongitude = usrLongitude;
+
+            };
+
+
+            vm.displayError = function (error) {
+
+                //get a reference to the HTML element for writing result
+                var locationElement = document.getElementById("locationData");
+
+                //find out which error we have, output message accordingly
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        locationElement.innerHTML = "Permission was denied";
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        locationElement.innerHTML = "Location data not available";
+                        break;
+                    case error.TIMEOUT:
+                        locationElement.innerHTML = "Location request timeout";
+                        break;
+                    case error.UNKNOWN_ERROR:
+                        locationElement.innerHTML = "An unspecified error occurred";
+                        break;
+                    default:
+                        locationElement.innerHTML = "Who knows what happened...";
+                        break;
+                }
+            };
         }]);
